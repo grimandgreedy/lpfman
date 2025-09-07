359eee31c79f47e8835032217a0016d964d44bc0
grimandgreedy <grimandgreedy@protonmail.com>
2025-09-08 02:34:03 +1000

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+"""
+setup.py
+
+Author: GrimAndGreedy
+Created: 2025-09-07
+License: MIT
+"""
+
+import setuptools
+
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "lpfman",
+    version = "0.1.0.1",
+    author = "Grim",
+    author_email = "grimandgreedy@protonmail.com",
+    description = "lpfman is a TUI file manager with extensive column support.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/grimandgreedy/lpfman",
+    project_urls = {
+        "Bug Tracker": "https://github.com/grimandgreedy/lpfman/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.9",
+
+    entry_points={
+        'console_scripts': [
+            'lpfman = lpfman:main',
+        ]
+    },
+    install_requires = [
+        "toml",
+        "listpick >= 0.1.16.1",
+    ],
+)
